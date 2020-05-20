9a0e79f01440f82753e9bbce1022ed46a0bbe8b7
nadzhou <ahmaddnavid@gmail.com>
2020-05-20 14:59:14 +0500

    fixed the parse_variation file

--- scripts/parse_variation.py
+++ scripts/parse_variation.py
@@ -12,20 +12,18 @@ from collections import Counter
 
 import sys
 
-    class DivergenceParser: 
-        """Parse the globally aligned sequences to look for divergence"""
+class DivergenceParser: 
+    """Parse the globally aligned sequences to look for divergence"""
 
-    def retrieve_dna(aligned_path): 
+    def retrieve_dna(self, aligned_path): 
         records = list(SeqIO.parse(aligned_path, "fasta"))
 
 
         seqs = [[x for x in y] for y in records]
 
-
-
         return seqs
 
-    def seq2np(seq): 
+    def seq2np(self, seq): 
         """"Turn the sequence into numpy S1 array for calculations later. 
 
         Args: 
@@ -40,7 +38,7 @@ import sys
 
 
 
-    def normalize_data(ent_list): 
+    def normalize_data(self, ent_list): 
         """Takes the entropy array and normalizes the data. 
 
         Args: 
@@ -57,7 +55,7 @@ import sys
 
 
 
-    def _shannon( array): 
+    def _shannon(self, array): 
         """Calculate Shannon Entropy vertically via loop. 
 
         Args: 
@@ -77,7 +75,7 @@ import sys
         return -np.sum(pA*np.log2(pA))
 
 
-    def conservation_score(np_seq): 
+    def conservation_score(self, np_seq): 
         """Calculate the Shannon Entropy vertically
         for each position in the amino acid msa sequence.
 
@@ -89,11 +87,11 @@ import sys
             scores vertically into a float nd array   
         """
 
-        return np.apply_along_axis(_shannon, 0, np_seq)  
+        return np.apply_along_axis(self._shannon, 0, np_seq)  
 
 
 
-    def label_plot(norm_list, norm_list_len, val, ax): 
+    def label_plot(self, norm_list, norm_list_len, val, ax): 
         a = np.concatenate({'x': norm_list_len, 'y': norm_list, 'val': val}, axis=1)
         
         for i, point in a.iteritems(): 
@@ -104,7 +102,7 @@ import sys
 
 def main(): 
     # Load the aligned FASTA file
-    aligned_path = "gisaid_results/mafft_aligned.fasta"
+    aligned_path = "gisaid_results/aligned.fasta"
 
     # Call an instance of the class
     inst = DivergenceParser()
@@ -138,8 +136,6 @@ def main():
             print(aa_s)
             ax.text(x,y, len(aa_s), color='b')
     plt.show()
-    
-
 
 
 if __name__ == '__main__': 
