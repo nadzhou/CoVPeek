86af0806224e6019c7a6b6b72cfd46ac4d211d2a
nadzhou <ahmaddnavid@gmail.com>
2020-05-24 15:14:29 +0500

    corrected the graphs.

--- scripts/parse_variation.py
+++ scripts/parse_variation.py
@@ -139,7 +139,7 @@ def main():
         Loads the aligned FASTA file, plots the variation score for each AA position,
         prints aminoacids in positions with score > 5, and makes a countplot for position 614
     """
-    aligned_path = "gisaid_results/aligned.fasta"
+    aligned_path = "/home/nadzhou/SEQs/gisaid_results/aligned.fasta"
     # Call an instance of the class that converts it to ndarray then run through the functions, calculate entropy etc
     parser = DivergenceParser.retrieve_sequence(aligned_path)
     lineplot = parser.plot_variation()
