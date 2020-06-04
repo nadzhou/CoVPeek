22178a7c5e00a8faeb902b62f120adcb90d3a3ba
nadzhou <ahmaddnavid@gmail.com>
2020-06-04 15:29:22 +0500

    Fixed the graph printing wrong number of amino acids

--- scripts/parse_variation.py
+++ scripts/parse_variation.py
@@ -23,6 +23,7 @@ class DivergenceParser:
         if sequences is None:
             sequences = []
         self.npseqs = self._seq2np(sequences)
+        print(self.npseqs[:, 614])
         self._conservation_scores = None
 
     @classmethod
@@ -79,7 +80,7 @@ class DivergenceParser:
 
         aa_count = Counter(array)
         pA = 1
-        total_aminoacids = 20
+        total_aminoacids = sum(aa_count.values())
         for aminoacid, count in aa_count.items():
             # and +1 for deletions
             pA *= (count / total_aminoacids + 1)
@@ -97,6 +98,8 @@ class DivergenceParser:
         if self._conservation_scores is None:
             conservation_scores = np.apply_along_axis(self._shannon, 0, self.npseqs)
             conservation_scores = self._normalize_data(conservation_scores)
+            print(conservation_scores[614])
+
             self._conservation_scores = conservation_scores
         return self._conservation_scores
 
@@ -107,21 +110,21 @@ class DivergenceParser:
                 fig: Variation score lineplot for a given sequence
         """
         scores = self.conservation_scores()
-        aa_positions = np.arange(1, len(scores) + 1)
+        aa_positions = np.arange(len(scores))
         # Now plot the data
         fig = plt.figure(figsize=(15, 10))
         ax = fig.add_subplot()
-        sns.lineplot(x=aa_positions, y=scores, color="purple", ax=ax)
+        sns.lineplot(x=aa_positions, y=scores, ax=ax)
         ax.set_xlabel("Amino acid position", fontsize=14)
         ax.set_ylabel("Variation score", fontsize=14)
         ax.set_title("Variation in the CoV spike protein", fontsize=14)
 
         minimum_labeled_variance = 5
 
-        for position, score, aminoacids in zip(aa_positions, scores, self.npseqs):
+        for position, score, aminoacids in zip(aa_positions, scores, self.npseqs.T):
             if score > minimum_labeled_variance:
-                aminoacids = self.npseqs[:, position-1].astype(str)
-                different_aas = set(aminoacids)
+                different_aas = set(aminoacids.astype(str))
+                print(aminoacids.astype(str))
                 ax.text(position, score, len(different_aas), color='b')
         return fig
 
@@ -141,14 +144,14 @@ def main():
         Loads the aligned FASTA file, plots the variation score for each AA position,
         prints aminoacids in positions with score > 5, and makes a countplot for position 614
     """
-    aligned_path = "/home/nadzhou/Desktop//6x2c/aligned_seq.fasta"
+    aligned_path = "aligned.fasta"
     # Call an instance of the class that converts it to ndarray then run through the functions, calculate entropy etc
     parser = DivergenceParser.retrieve_sequence(aligned_path)
     lineplot = parser.plot_variation()
     #plt.show()
     variable_positions = parser.aminoacids_in_variable_positions()
     print_variable(variable_positions)
-    # countplots = make_countplots(variable_positions[614])
+    #countplots = make_countplots(variable_positions[203])
     plt.show()
 
 
