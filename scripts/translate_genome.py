5a178b9a74c384fd600863125e19a44ed59f86f6
nadzhou <ahmaddnavid@gmail.com>
2020-06-21 18:17:01 +0500

    fixed a bit of translate

--- scripts/translate_genome.py
+++ scripts/translate_genome.py
@@ -89,7 +89,6 @@ def find_orfs_with_trans(seq: Seq, trans_table: int = 1, min_protein_length: int
     """
     answer = []
     seq = pad_seq(seq)
-    seq_len = len(seq)
 
     for strand, nuc in [(+1, seq), (-1, seq.reverse_complement())]:
         for frame in range(3):
@@ -97,19 +96,16 @@ def find_orfs_with_trans(seq: Seq, trans_table: int = 1, min_protein_length: int
             trans_len = len(trans)
             aa_start = 0
             aa_end = 0
+
             while aa_start < trans_len:
                 aa_end = trans.find("*", aa_start)
                 if aa_end == -1:
                     aa_end = trans_len
+
                 if aa_end - aa_start >= min_protein_length:
-                    if strand == 1:
-                        start = frame + aa_start * 3
-                        end = min(seq_len, frame + aa_end * 3 + 3)
-                    else:
-                        start = seq_len - frame - aa_end * 3 - 3
-                        end = seq_len - frame - aa_start * 3
                     answer.append(trans[aa_start:aa_end])
                 aa_start = aa_end + 1
+                
     return answer
 
 
